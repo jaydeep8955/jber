88e725e40b8751751ec514010a6848b8d6ad87e9
jaydeep8955 <jaydeepsabhad24@gmail.com>
2024-11-14 08:10:09 +0000

    inital commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const { createServer } = require('node:http');
+
+// const hostname = 'localhost';
+const port = 3004;
+
+const server = createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('node push project updated');
+});
+
+server.listen(port, () => {
+    console.log(`Server running at http://localhost:${potrt}/`);
+});
